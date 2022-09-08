49096ddfffba003e8084a1f35eb83bbc4988ab95
Kyunghwan Kwon <kwon@toanyone.net>
2022-09-08 16:47:02 +0900

    ports: esp-idf: fix the project information to be used by idf.py

--- ports/esp-idf/esp32.cmake
+++ ports/esp-idf/esp32.cmake
@@ -18,10 +18,18 @@ idf_build_process(${DEVICE}
 		${CMAKE_CURRENT_BINARY_DIR}
 )
 
+# project_description.json metadata file used for the flash and the monitor of
+# idf.py to get the project information.
+set(PROJECT_EXECUTABLE ${CMAKE_PROJECT_NAME}.elf)
+set(PROJECT_BIN ${CMAKE_PROJECT_NAME}.bin)
+set(build_components_json "[]")
+set(build_component_paths_json "[]")
+configure_file("${IDF_PATH}/tools/cmake/project_description.json.in"
+	"${CMAKE_CURRENT_BINARY_DIR}/project_description.json")
+
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-set(elf_file ${CMAKE_PROJECT_NAME}.elf)
-add_executable(${elf_file}
+add_executable(${PROJECT_EXECUTABLE}
 	${CMAKE_CURRENT_LIST_DIR}/start.c
 	${CMAKE_CURRENT_LIST_DIR}/system.c
 	${CMAKE_CURRENT_LIST_DIR}/uart0.c
@@ -29,18 +37,18 @@ add_executable(${elf_file}
 	${APP_SRCS}
 )
 
-target_compile_options(${elf_file} PRIVATE ${compile_options})
-target_link_options(${elf_file} PRIVATE -Wl,--print-memory-usage)
-target_include_directories(${elf_file}
+target_compile_options(${PROJECT_EXECUTABLE} PRIVATE ${compile_options})
+target_link_options(${PROJECT_EXECUTABLE} PRIVATE -Wl,--print-memory-usage)
+target_include_directories(${PROJECT_EXECUTABLE}
 	PRIVATE
 		${APP_INCS}
 		$ENV{IDF_PATH}/components/freertos/FreeRTOS-Kernel/include/freertos
 		$ENV{IDF_PATH}/components/freertos/include/freertos
 )
-target_compile_definitions(${elf_file} PRIVATE ${APP_DEFS})
+target_compile_definitions(${PROJECT_EXECUTABLE} PRIVATE ${APP_DEFS})
 
 # Link the static libraries to the executable
-target_link_libraries(${elf_file}
+target_link_libraries(${PROJECT_EXECUTABLE}
 	idf::${DEVICE}
 	idf::freertos
 	idf::spi_flash
@@ -48,11 +56,11 @@ target_link_libraries(${elf_file}
 )
 
 set(mapfile "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.map")
-target_link_libraries(${elf_file}
+target_link_libraries(${PROJECT_EXECUTABLE}
 	"-Wl,--cref"
 	"-Wl,--Map=\"${mapfile}\""
 )
 
 # Attach additional targets to the executable file for flashing,
 # linker script generation, partition_table generation, etc.
-idf_build_executable(${elf_file})
+idf_build_executable(${PROJECT_EXECUTABLE})
