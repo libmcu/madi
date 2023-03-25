075c15f4f45b9e5c5fa0cdfd41896840581751dd
Kyunghwan Kwon <k@mononn.com>
2023-03-26 04:47:30 +0900

    madi-stm32: add jlink flash target

--- projects/boards/madi_stm32.cmake
+++ projects/boards/madi_stm32.cmake
@@ -7,7 +7,8 @@ set(PLATFORM_SPECIFIC_DIR ${CMAKE_SOURCE_DIR}/ports/stm32/g4)
 
 add_custom_target(${PROJECT_NAME}.bin ALL DEPENDS ${PROJECT_NAME})
 add_custom_target(${PROJECT_NAME}.hex ALL DEPENDS ${PROJECT_NAME})
-add_custom_target(flash DEPENDS ${PROJECT_NAME}.hex)
+add_custom_target(flash DEPENDS ${PROJECT_NAME}.bin)
+add_custom_target(flash_jlink DEPENDS ${PROJECT_NAME}.bin)
 add_custom_target(gdb DEPENDS ${PROJECT_NAME})
 
 add_custom_command(TARGET ${PROJECT_NAME}.hex
@@ -25,6 +26,14 @@ add_custom_command(TARGET ${PROJECT_NAME}.bin
 add_custom_command(TARGET flash
 	USES_TERMINAL COMMAND
 		dfu-util --alt 0 --dfuse-address 0x08000000 --download ${PROJECT_NAME}.bin
+)
+
+add_custom_command(TARGET flash_jlink
+	USES_TERMINAL
+	COMMAND
+		echo \"r\\nloadbin ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.bin, 0x08000000\\nr\\nq\" > ${PROJECT_NAME}.jlink
+	COMMAND
+		JLinkExe -if swd -device stm32g473ce -speed 4000 -CommanderScript ${PROJECT_NAME}.jlink
 )
 
 add_custom_command(TARGET gdb
--- projects/boards/madi_stm32.mk
+++ projects/boards/madi_stm32.mk
@@ -7,9 +7,13 @@ INCS += $(PORT_ROOT)
 
 include projects/arch/cm4f.mk
 
-.PHONY: flash erase gdbserver
+.PHONY: flash flash_jlink erase gdbserver
 flash: $(OUTBIN)
 	dfu-util --alt 0 --dfuse-address 0x08000000 --download $<
+flash_jlink: $(OUTDIR)/$(BOARD).jlink
+	JLinkExe -if swd -device stm32g473ce -speed 4000 -CommanderScript $<
+$(OUTDIR)/$(BOARD).jlink: $(OUTBIN)
+	echo "r\nloadbin $<, 0x08000000\nr\nq" > $@
 erase:
 	dfu-util --alt 0 --dfuse-address 0x08000000:mass-erase:force
 gdbserver:
